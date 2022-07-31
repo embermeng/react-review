9988ddabad2468054ec235212420a8bf5d1a9c40
embermeng <embermeng@gmail.com>
2022-07-31 20:10:53 +0800

    update

--- 03_react_app/src_10_RTKQ未完成/index.js
+++ 03_react_app/src_10_RTKQ未完成/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// store数据注入
-import { Provider } from 'react-redux'
-import store from './store'
 
 // 设置移动端的适配-rem
 document.documentElement.style.fontSize = 100 / 750 + 'vw' // 设置视口宽度为750rem
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <App></App>
-    </Provider>
+    <App></App>
 );
